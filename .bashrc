8949ef3a662379afa2a683814fc8decd68a01724
shinpoi <petersbw123@gmail.com>
2018-04-11 12:28:14 +0900

    rebuild

---
+++ .bashrc
@@ -0,0 +1,6 @@
+alias ls='ls -G'
+alias grep='grep --color=auto'
+alias rm='rm -r'
+alias ll='ls -lh'
+alias la='ls -A'
+
---
+++ git_init.sh
@@ -0,0 +1,6 @@
+git config --global alias.cm "commit -m"
+git config --global alias.st status
+git config --global alias.all "add --all"
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
