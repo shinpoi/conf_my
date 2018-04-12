d8be15ebfb03f9ac453f9bb586c400941ca99749
shinpoi <petersbw123@gmail.com>
2018-04-12 10:56:13 +0900

    update color(bash)

--- .bashrc
+++ .bashrc
@@ -1,3 +1,6 @@
+if [ "$TERM" = xterm ]; then TERM=xterm-256color; fi
+PS1='\[\033[1;92m\]\u@\h\[\033[00m\]:\[\033[96m\]\w\[\033[00m\]\$ '
+
 alias ls='ls -G'
 alias grep='grep --color=auto'
 alias rm='rm -r'
--- git_init.sh
+++ git_init.sh
@@ -2,5 +2,5 @@ git config --global alias.cm "commit -m"
 git config --global alias.st status
 git config --global alias.all "add --all"
 
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
