21e1e12b8962c1df320e4ef8f610fbda281f837b
Aoi-lucario <petersbw123@gmail.com>
2018-05-31 16:50:00 +0900

    Update git_init.sh

--- git_init.sh
+++ git_init.sh
@@ -2,6 +2,8 @@ git config --global alias.cm "commit -m"
 git config --global alias.st status
 git config --global alias.all "add --all"
 git config --global alias.pm "push origin master"
+git config --global alias.ck checkout
+git config --global alias.br branch
 
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
