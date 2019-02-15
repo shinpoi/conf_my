b267bd8e936f068029e10121a79dbcc381d9a4e6
Aoi-lucario <petersbw123@gmail.com>
2019-02-15 13:00:46 +0900

    update conf for mac

--- git_init.sh
+++ git_init.sh
@@ -1,14 +1,16 @@
 # git setting: /etc/bash_completion.d/git
 
-git config --global alias.cm "commit -m"
-git config --global alias.st status
-git config --global alias.all "add --all"
-git config --global alias.pm "push origin master"
-git config --global alias.ck checkout
-git config --global alias.br branch
+git config --global alias.cm 'commit -m'
+git config --global alias.st 'status'
+git config --global alias.all 'add --all'
+git config --global alias.pm 'push origin master'
+git config --global alias.ck 'checkout'
+git config --global alias.br 'branch'
+git config --global alias.ri 'rebase -i'
+
 git config --global color.ui true
 
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lg 'log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
 
 # git config --global user.name "---"
 # git config --global user.email "---"
