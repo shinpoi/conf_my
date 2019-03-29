dec1669f225eada1c8047efd4ac548a6f7ad5a64
Aoi-lucario <petersbw123@gmail.com>
2019-03-29 15:45:38 +0900

    update .bashrc.mac

--- bashrc.mac.sh
+++ bashrc.mac.sh
@@ -10,7 +10,8 @@ alias ls='ls -G'
 alias ll='ls -lhG'
 alias la='ls -AG'
 PS1='\[\033[1;92m\]\u@DevMac\[\033[00m\]:\[\033[1;38;5;117m\]\w\[\033[00m\]\$ '
-export LSCOLORS=GxexFxfxcxDxdxCxChacad
+export LSCOLORS='GxexFxfxcxDxdxCxChacad'
 
 alias pyserver='python3 -m http.server 8080'
-alias DSdestory='function _del(){ rm -r "./$1/DS_Store" "./$1/**/.DS_Store"; };_del'
+alias DSdestory='function _delDS(){ find "./$1" -name ".DS_Store" -type f -print -delete; };_delDS'
+
