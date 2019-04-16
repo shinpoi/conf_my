575f4c6d37649d6ba1951099778178b47853f699
Aoi-lucario <petersbw123@gmail.com>
2019-04-16 15:28:19 +0900

    fix PS1

--- src/bashrc.linux.sh
+++ src/bashrc.linux.sh
@@ -1,5 +1,5 @@
 ## Linux
-export PS1='\e[1;96m\u@\h\e[00m:\e[36m\w\e[00m\$ '
+export PS1='\[\e[1;96m\]\u@\h\[\e[00m\]:\[\e[36m\]\w\[\e[00m\]\$ '
 export LS_COLORS=$LS_COLORS:"fi=00:di=36:ln=01;36:pi=00;42:so=00;44:bd=01;93:cd=00;93:or=01;31:mi=01;90:ex=00;92:*.tar=00;90:*.gz=00;90:*.7z=00;90:*.zip=00;90:*.png=00;36:*.jpg=00;36:*.bmp=00;36:*.gif=00;36:"
 
 alias ls='ls --color'
